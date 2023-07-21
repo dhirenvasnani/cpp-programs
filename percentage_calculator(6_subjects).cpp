f822724b726986d5ebcfe4b4beb465aeb58328d3
DhirenV <87919051+dhirenvasnani@users.noreply.github.com>
2023-07-21 21:13:11 +0530

    Update percentage_calculator(6_subjects).cpp

--- percentage_calculator(6_subjects).cpp
+++ percentage_calculator(6_subjects).cpp
@@ -1,5 +1,7 @@
+void execute();
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
 
 class result {
@@ -55,7 +57,7 @@ class result {
 			cout<<endl<<"------------------------"<<endl;
 			cout<<"Marks Scored:- \n"<<"1. English: "<<english<<endl<<"2. Maths: "<<maths<<endl<<"3. Computer Science Paper 1: "<<cs1<<endl<<"4. Physics: "<<physics<<endl<<"5. Chemistry: "<<chemistry<<endl<<"6. Computer Science Paper 2: "<<cs2<<endl;
 			cout<<endl<<"------------------------"<<endl;
-			cout<<"Total Marks: "<<tot;
+			cout<<"Total Marks (Out of 500): "<<tot;
 			cout<<endl<<"------------------------"<<endl;
 			cout<<"Percetage: "<<percent<<"%";
 		    cout<<endl<<"------------------------"<<endl;	
@@ -79,36 +81,46 @@ class result {
 		cout<<"Result saved as "<<name<<" "<<surname<<" Result.txt";
 		cout<<endl<<"------------------------"<<endl;
 	}
+	
+	void loop() {
+		char choice;
+		cout<<"Do you want to run the program again (y/n)? ";
+		cin>>choice;
+		
+		if (choice == 'y') {
+			execute();
+		} else {
+			exit(0);
+		}
+	}
 };
 
 int main() {
-	char y, save;
+	execute();
+	return 0;
+}		
+		
+void execute() {
 	result obj;
 	obj.getdata();
 	obj.calculate();
 	obj.display();
 	
+	char save;
 	cout<<"Do you want to save the result (y/n)? ";
 	cin>>save;
+	cout<<"------------------------"<<endl;
+	
 	if (save == 'y') {
 	    obj.save();
+	    obj.loop();
 	} else if (save == 'n') {
 	    cout<<"Result not saved.";
+	    cout<<endl<<"------------------------"<<endl;
+	    obj.loop();
 	} else {
 	    cout<<"Error: Invalid Input";
+	    cout<<endl<<"------------------------"<<endl;
+	    obj.loop();
 	}
-	
-	
-	cout<<endl<<"Do you want to run the program again (y / n)? ";
-	cin>>y;
-		if (y == 'y') {
-		    obj.getdata();
-		    obj.calculate();
-		    obj.display();
-		    obj.save();
-		} else {
-		    cout<<"[Program Finished]";
-		}
-	return 0;
-}		
-		
+}
